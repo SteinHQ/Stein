dbf37b22e0d034650cdf339901a1e102bda0317d
Shiven Sinha <shivensinha4@gmail.com>
2021-07-22 14:46:10 +0530

    Updated all packages to latest version, hence also added {useUnifiedTopology: true} to mongoose connect options in db.js

--- helpers/db.js
+++ helpers/db.js
@@ -1,12 +1,13 @@
 const Mongoose = require("mongoose").Mongoose,
-  dotenv = require("dotenv"),
-  path = require("path");
+    dotenv = require("dotenv"),
+    path = require("path");
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({path: path.resolve(__dirname, "../.env")});
 
 const dbConnection = new Mongoose();
 
 dbConnection.Promise = require("bluebird");
-dbConnection.connect(process.env.STEIN_MONGO_URL, { useNewUrlParser: true });
+dbConnection.connect(process.env.STEIN_MONGO_URL,
+    {useNewUrlParser: true, useUnifiedTopology: true});
 
 module.exports = dbConnection;
